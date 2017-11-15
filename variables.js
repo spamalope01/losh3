a3778907f559b4e821a8dd7830aee70b0999fce4
Jonathan Daniel <jonathandaniel@Jonathans-MacBook-Pro.local>
2017-11-14 20:27:38 -0800

    1

---
+++ variables.js
@@ -0,0 +1,21 @@
+// (function(module){
+//
+//
+//
+// mail.$$password = "stone905";
+//
+//
+//
+//
+//
+//
+// module.password = pass;
+// })(window);
+
+
+
+
+// mymodule.js
+module.exports = {
+   password: "stone905"
+   }
