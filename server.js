c01186dd3c85993a196af9cc243057a12856c3f4
Jonathan Daniel <jonathandaniel@Jonathans-MacBook-Pro.local>
2017-11-14 17:52:05 -0800

    6

--- server.js
+++ server.js
@@ -1,5 +1,36 @@
+// var express = require('express');
+// var app = express();
+// var port = process.env.PORT || 9000;
+//
+// app.use(express.static(__dirname + '/app'));
+//
+//
+// app.get('/', function(req, res) {
+//   res.sendFile(__dirname + '/index.html');
+// });
+//
+// app.listen(port, function(){
+//   console.log('Listening on port:', port);
+// });
+
+
+
 var express = require('express');
+var nodemailer = require('nodemailer');
 var app = express();
+
+const password = require('./variables');
+let pass = password.password;
+
+
+var smtpTransport = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  // port: 587,
+  auth: {
+    user: "l2workquery@gmail.com",
+    pass: pass
+  }
+});
 var port = process.env.PORT || 9000;
 
 app.use(express.static(__dirname + '/app'));
@@ -9,57 +40,26 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/send', function(req, res) {
+  var mailOptions={
+    to : "l2workquery@gmail.com",
+    subject: "Inquiry about work.",
+    from: req.query.email,
+    phone: req.query.phone,
+    email: req.query.email,
+    text : "You have received a project request. \n \n Contact Name: " + req.query.from + ". \n \n Contact email: " + req.query.email + ". \n \n Contact phone: " + req.query.phone + " \n \n Message: " + req.query.text
+  };
+  smtpTransport.sendMail(mailOptions, function(error, response){
+    if(error){
+      console.log('there was a problem', error);
+      res.end("error");
+    }else{
+      console.log("Message sent: " + res.message);
+      res.end("sent");
+    }
+  });
+});
+
 app.listen(port, function(){
   console.log('Listening on port:', port);
 });
-
-
-
-// var express = require('express');
-// var nodemailer = require('nodemailer');
-// var app = express();
-
-// const password = require('./variables');
-// let pass = password.password;
-
-
-// var smtpTransport = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   // port: 587,
-//   auth: {
-//     user: "l2workquery@gmail.com",
-//     pass: pass
-//   }
-// });
-// var port = process.env.PORT || 9000;
-//
-// app.use(express.static(__dirname + '/app'));
-//
-//
-// app.get('/', function(req, res) {
-//   res.sendFile(__dirname + '/index.html');
-// });
-
-// app.get('/send', function(req, res) {
-//   var mailOptions={
-//     to : "l2workquery@gmail.com",
-//     subject: "Inquiry about work.",
-//     from: req.query.email,
-//     phone: req.query.phone,
-//     email: req.query.email,
-//     text : "You have received a project request. \n \n Contact Name: " + req.query.from + ". \n \n Contact email: " + req.query.email + ". \n \n Contact phone: " + req.query.phone + " \n \n Message: " + req.query.text
-//   };
-//   smtpTransport.sendMail(mailOptions, function(error, response){
-//     if(error){
-//       console.log('there was a problem', error);
-//       res.end("error");
-//     }else{
-//       console.log("Message sent: " + res.message);
-//       res.end("sent");
-//     }
-//   });
-// });
-
-// app.listen(port, function(){
-//   console.log('Listening on port:', port);
-// });
